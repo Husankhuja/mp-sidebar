68d98c75cda2874736d17d6012ab0a106d914801
Carlos Gomez <CGomez374@yahoo.com>
2021-05-10 11:10:42 -0700

    created sidebar using next.js and moved profile component over

---
+++ components/SideBar/index.jsx
@@ -0,0 +1,10 @@
+import Profile from '../Profile/index'
+
+export default function Index() {
+    return (
+      <div>
+        <h1>hello from sidebar</h1>
+        <Profile />
+      </div>
+    )
+  }
