b5db64779be236f5d898feef22293c44d61b41e4
husan <husan.2002.n@gmail.com>
2021-05-10 12:43:59 -0400

    added icons for profile

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Sidebar from '../sidebar/Sidebar';
+import SideBar from '../sidebar/Sidebar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <SideBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Sidebar.css';
+import '../profile/Profile';
+import Profile from '../profile/Profile';
+
+
+const SideBar = () => {
+    return (
+        <div className="sidebar">
+            {
+                //sidebar header
+                //logo
+                //minority programmers
+            }
+            <Profile />
+            {
+                // Button
+                // List
+            }
+        </div>
+    );
+}
+
+export default SideBar;
