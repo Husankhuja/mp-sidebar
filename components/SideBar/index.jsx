58be256034106f58e9c162a3728d044f708b756c
Husan <husan.2002.n@gmail.com>
2021-05-18 12:19:45 -0400

    sidebar collopses on small screens

--- components/SideBar/index.jsx
+++ components/SideBar/index.jsx
@@ -2,6 +2,7 @@ import SideBarHeader from '../SideBarHeader/index';
 import Profile from '../Profile/index';
 import Button from '../Button/index';
 import SideBarNav from '../SideBarNav/index';
+import { useState } from 'react';
 import ListOfLinks from '../../public/Links/ListOfLinks';
 import userData from '../../public/userData';
 
@@ -9,9 +10,10 @@ import userData from '../../public/userData';
 import Styles from './sidebar.module.css';
 
 
-export default function Index() {
+export default function SideBar() {
+  const [display, setDisplay] = useState(false);
   return (
-    <div className={Styles.sidebar}>
+    <div className={display === true ? Styles.sidebar : `${Styles.sidebar} ${Styles.sidebarDisplay}`}>
       <SideBarHeader />
       <Profile userData={userData} />
       <Button />
